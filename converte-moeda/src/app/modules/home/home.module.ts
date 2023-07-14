de0ae953457e6da68ce3d764990724ebf9aacde8
Gabrieli Ribeiro <97065013+gabrieliribeiro@users.noreply.github.com>
2023-07-14 11:58:54 -0300

    Enviando

--- converte-moeda/src/app/modules/home/home.module.ts
+++ converte-moeda/src/app/modules/home/home.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './components/form/form.component';
-import { ResutsComponent } from './components/resuts/resuts.component';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { AgrupadorComponent } from './pages/home/agrupador/agrupador.component';
@@ -10,7 +9,6 @@ import { AgrupadorComponent } from './pages/home/agrupador/agrupador.component';
 @NgModule({
   declarations: [
     FormComponent,
-    ResutsComponent,
     AgrupadorComponent
   ],
   imports: [
