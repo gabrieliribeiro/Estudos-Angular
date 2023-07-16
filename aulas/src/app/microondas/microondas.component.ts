7b2fee8bb8a906e9e5759faf1c6beb35fe0c84de
gabrieliribeiro <gabrielirt@hotmail.com>
2023-07-16 20:26:10 -0300

    Enviando exercicio do microondas corrigido

--- aulas/src/app/microondas/microondas.component.ts
+++ aulas/src/app/microondas/microondas.component.ts
@@ -3,39 +3,56 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-microondas',
   templateUrl: './microondas.component.html',
-  styleUrls: ['./microondas.component.scss']
+  styleUrls: ['./microondas.component.scss'],
 })
-export class MicroondasComponent{
+export class MicroondasComponent {
   time: number = 0;
-  number: number = 0;
+  selectedDigits: string = '';
+  maxTime: number = 5999;
   isPaused: boolean = false;
-  maxTime: number = 5940; 
-  
+
   formatTime(): string {
-  const minutes: number = Math.floor(this.time / 60);
-  const seconds: number = this.time % 60;
+    const minutes: number = Math.floor(this.time / 60);
+    const seconds: number = this.time % 60;
 
-  const formattedMinutes: string = String(minutes).padStart(2, '0');
-  const formattedSeconds: string = String(seconds).padStart(2, '0');
+    const formattedMinutes: string = String(minutes).padStart(2, '0');
+    const formattedSeconds: string = String(seconds).padStart(2, '0');
 
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
-
-
-addDigit(digit: number) {
-  const newTime = this.time * 10 + digit;
-  if (newTime <= this.maxTime) {
-    this.time = newTime;
+    return `${formattedMinutes}:${formattedSeconds}`;
   }
-}
 
-addTime(seconds: number) {
-  const newTime = this.time + seconds;
-  if (newTime <= this.maxTime) {
-    this.time = newTime;
+  addDigit(digit: number): void {
+    const newTime = this.time * 10 + digit;
+    if (this.selectedDigits.length < 4) {
+      this.selectedDigits += digit.toString();
+      if (
+        this.selectedDigits.length === 1 ||
+        this.selectedDigits.length === 2
+      ) {
+        this.time = +this.selectedDigits;
+        if (this.time > 99) {
+          this.time = 99;
+          }
+      } else if (
+        this.selectedDigits.length === 3 ||
+        this.selectedDigits.length === 4
+      ) {
+        const minutes = Math.floor(+this.selectedDigits / 100);
+        const seconds = +this.selectedDigits % 100;
+        this.time = minutes * 60 + seconds;
+        if (this.time > this.maxTime) {
+          this.time = this.maxTime;
+          }
+      }
+    }
   }
-}
 
+  addTime(seconds: number): void {
+    const newTime = this.time + seconds;
+    if (newTime <= this.maxTime) {
+      this.time = newTime;
+    }
+  }
 
   startMicrowave() {
     const countdown = setInterval(() => {
@@ -52,6 +69,7 @@ addTime(seconds: number) {
   }
 
   cancelMicrowave() {
+    this.selectedDigits = '';
     this.time = 0;
   }
 }
